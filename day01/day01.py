4d50c4de9ef266f910cab289961ea221e1871349
Renan Silva <renansupermanox@gmail.com>
2024-09-19 14:32:29 +0100

    improvement in day01 - part 2

--- day01/day01.py
+++ day01/day01.py
@@ -20,7 +20,6 @@ yOffset = 0
 
 for instruction in line:
     facing = (facing + (1 if instruction[0] == "R" else -1)) % 4
-
     blocks = int(instruction[1:])
 
     if facing % 2 == 0: # facing north or south
@@ -36,50 +35,33 @@ facing = 0
 xOffset = 0
 yOffset = 0
 visited_locations = set()
-visited_locations.add((xOffset, yOffset))  # Adicionar o ponto inicial
+visited_locations.add((xOffset, yOffset))
 visited_twice = ()
 
 for instruction in line:
     facing = (facing + (1 if instruction[0] == "R" else -1)) % 4
-
     blocks = int(instruction[1:])
+    incrementer = ()
 
     if facing == 0:
-        for i in range(blocks):
-            yOffset += 1
-            coords = (yOffset, xOffset)
-            if coords not in visited_locations:
-                visited_locations.add(coords)
-            else:
-                visited_twice = coords
-                break
+        incrementer = (0, 1)
     elif facing == 1:
-        for i in range(blocks):
-            xOffset += 1
-            coords = (yOffset, xOffset)
-            if coords not in visited_locations:
-                visited_locations.add(coords)
-            else:
-                visited_twice = coords
-                break
+        incrementer = (1, 0)
     elif facing == 2:
-        for i in range(blocks):
-            yOffset -= 1
-            coords = (yOffset, xOffset)
-            if coords not in visited_locations:
-                visited_locations.add(coords)
-            else:
-                visited_twice = coords
-                break
+        incrementer = (0, -1)
     else:
-        for i in range(blocks):
-            xOffset -= 1
-            coords = (yOffset, xOffset)
-            if coords not in visited_locations:
-                visited_locations.add(coords)
-            else:
-                visited_twice = coords
-                break
+        incrementer = (-1, 0)
+
+    for _ in range(blocks):
+        xOffset += incrementer[0]
+        yOffset += incrementer[1]
+        coords = (xOffset, yOffset)
+
+        if coords in visited_locations:
+            visited_twice = coords
+            break
+        else:
+            visited_locations.add(coords)
 
     if visited_twice:
         break
